52b7aa5b9cbc26475c77de0eb110c17fd857e821
Keodina <keodinachey2011@gmail.com>
2017-07-13 16:12:21 +0700

    User Delete thread

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,25 +4,21 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Forum Threads</div>
-                    <div class="panel-body">
-                        @foreach($threads as $thread)
-                            <article>
-                                <div class="level" style="display: flex;align-items: center">
-                                    <h4 class="flex" style="flex: 1">
-                                        <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                    </h4>
-                                    <a href="{{ $thread->path() }}"><strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong></a>
-                                </div>
-                                <div>
-                                    {{ $thread->body }}
-                                </div>
-                            </article>
-                            <hr>
-                        @endforeach
+                @foreach($threads as $thread)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="level" style="display: flex;align-items: center">
+                                <h4 class="flex" style="flex: 1">
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                </h4>
+                                <a href="{{ $thread->path() }}"><strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong></a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            {{ $thread->body }}
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
